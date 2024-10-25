7484bb57c169b18e5daa97c41220854480d441c4
binoo <binoo@DESKTOP-79EU623>
2024-10-25 11:08:36 +0530

    updated

---
+++ JavaSampleMaven/src/main/java/floatad/Float_Addition.java
@@ -0,0 +1,16 @@
+package floatad;
+
+public class Float_Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			float a=12.5f;
+			float b=15.2f;
+			float c=a+b;
+			
+			System.out.println("The Sum Is "+c);
+			
+	}
+
+}
